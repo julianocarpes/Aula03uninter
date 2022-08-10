b6147947e9d64b49efbfe64012c26fc459c4cefb
jJuliano <Juliano.carpes@gmail.com>
2022-08-10 10:12:33 -0300

    construção das telas

---
+++ app/src/main/java/com/example/aula03uninter/EsqueciSenhaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aula03uninter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EsqueciSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_esqueci_senha)
+    }
+}
--- app/src/main/java/com/example/aula03uninter/MainActivity.kt
+++ app/src/main/java/com/example/aula03uninter/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.aula03uninter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener{
+            startActivity(Intent(this,HomeActivity::class.java))
+        }
+        findViewById<TextView>(R.id.esqueciSenha).setOnClickListener{
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        }
+        findViewById<TextView>(R.id.Cadastrar).setOnClickListener{
+            startActivity(Intent(this,CadastroActivity::class.java))
+        }
     }
 }
